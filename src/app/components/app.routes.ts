754c7e0e7552a8f9500268a505e6a59978e6c1e7
Carlos Carballo <ballokalitos@gmail.com>
2018-05-04 13:49:32 -0500

    Menu modificado

--- src/app/components/app.routes.ts
+++ src/app/components/app.routes.ts
@@ -8,10 +8,10 @@ PortfolioItemComponent
 
 
 const app_routes: Routes = [
-  { path: '', component: PortafolioComponent },
+  { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'producto', component: PortfolioItemComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
-export const app_routing = RouterModule.forRoot(app_routes);
+export const app_routing = RouterModule.forRoot(app_routes, { useHash:true});
